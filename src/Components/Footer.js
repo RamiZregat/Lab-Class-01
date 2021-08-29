1fffb655d542bcc496ea2525c301782576e03f74
RamiZregat <rami.zregat97@gmail.com>
2021-08-29 18:31:52 +0300

    finished

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Footer extends React.Component{
+    render(){
+        return(
+            <>
+            <h3>Rami Zregat</h3>
+            </>
+        )
+    }
+}
+export default Footer;
